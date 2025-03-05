4197ca6e192473854e6643872cc2ecbe7bcc37fd
Madhavgarg98 <madhavgarg3300@gmail.com>
2025-03-05 10:41:20 +0530

    setup-done-8

--- backend/middleware/db.js
+++ backend/middleware/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../config/.env") });
 
 const connectDatabase = () => {
   mongoose
