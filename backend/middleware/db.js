de33e355c0a608a83fef3421fd2a34e0c7e9e190
Madhavgarg98 <madhavgarg3300@gmail.com>
2025-04-17 14:30:57 +0530

    task-15

--- backend/middleware/db.js
+++ backend/middleware/db.js
@@ -4,10 +4,7 @@ require("dotenv").config({ path: path.resolve(__dirname, "../config/.env") });
 
 const connectDatabase = async () => {
   try {
-    const data = await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const data = await mongoose.connect(process.env.DB_URL)
     console.log(`✅ MongoDB connected: ${data.connection.host}`);
   } catch (err) {
     console.error("❌ MongoDB connection error:", err);
